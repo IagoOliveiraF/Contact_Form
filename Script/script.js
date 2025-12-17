c969c34fe9d8346da495147f7d11f52b0dd1b4b2
Iago Oliveira <iagooliveirafonseca84@gmail.com>
2025-12-17 10:51:51 -0300

    finalizando os elementos de interatividade (Hover - Focus ) e iniciando a lógica

---
+++ Script/script.js
@@ -0,0 +1,16 @@
+function loadPage() {
+    let form = document.querySelector('form')[0];
+    let allMessageErrorElements = document.querySelectorAll('.MessageError')
+    console.log(allMessageErrorElements)
+    form.addEventListener('submit', function(event){
+        event.preventDefault()
+
+        
+
+
+    })
+}
+
+
+
+
